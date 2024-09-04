404dbe3b37ee897f4977bf30d1e7400e3fc93d98
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-09-04 09:24:38 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+// src/components/App.js
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import Header from "./Header";
+import itemData from "../data/items";
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function onDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  return (
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <Header onDarkModeClick={onDarkModeClick} isDarkMode={isDarkMode} />
+      <ShoppingList items={itemData} />
+    </div>
+  );
+}
+
+export default App;
